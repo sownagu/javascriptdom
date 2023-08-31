1f650963a960469799312847a98b2814566b3226
Gray angel <mrabu0101@gmail.com>
2023-08-31 13:08:43 +0530

    modified dom.js

--- dom.js
+++ dom.js
@@ -1,5 +1,14 @@
 //document.getElementById('main').innerHTML='hello world!';
 //document.getElementById('main').innerText='hello world!';
-
-
-document.querySelectorAll('.main').style.Color='green';
+let click=document.querySelector('.main');
+let clicks=()=>
+{
+if( click.style.backgroundColor!=='red')
+{
+ click.style.color='white';
+ click.style.backgroundColor='red';
+}
+else{
+    click.style.color='green';
+}};
+click.addEventListener('click',clicks);
