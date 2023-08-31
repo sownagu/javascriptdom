5f3bc4d4a25163ed4c08e3457e4317d1979a8092
Gray angel <mrabu0101@gmail.com>
2023-08-31 11:12:18 +0530

    modified js.html and dom.js

--- dom.js
+++ dom.js
@@ -1,2 +1,4 @@
 //document.getElementById('main').innerHTML='hello world!';
-document.getElementById('#main  h1').innerText='hello world!';
+//document.getElementById('main').innerText='hello world!';
+document.querySelector('#main p').innerText='hai';
+document.write('happy');
